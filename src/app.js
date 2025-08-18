d08051cc736f2318dde9edbf754d50a1ec268b41
Pavan-Kolhe <pavandigambarkolhe@gmail.com>
2025-08-18 14:51:32 +0530

    feat: set up Express server with CORS and body parsing middleware
    - Updated package.json to include nodemon for development and added express, cors, and dotenv as dependencies.
    - Created app.js to configure Express middleware for JSON and URL-encoded data handling, static file serving, and CORS settings.
    - Implemented basic routes for the root and Instagram page.
    - Added index.js to initialize the server and load environment variables from a .env file.

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+const app = express();
+
+//  app.use() -> middleware
+
+// basic config
+app.use(express.json({ limit: "16kb" })); // body parser to accpt json data
+app.use(urlencoded({ extended: true }, { limit: "16kb" })); // to accept urlencoded data
+app.use(express.static("public")); // to enable serving static files from public folder
+
+//cors cnfig
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:3000", // what url i will allowed to communicate with backend
+    credentials: true, // to accept cookies
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("welcome to projectCamp");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("this is an instagram page");
+});
+
+export default app;
